a4389289527787d0841b809c2533259ee7abeb7a
JackBDart <jack.w.benhayon.21@dartmouth.edu>
2021-05-28 13:45:27 -0700

    user actions

---
+++ src/actions/user.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { AsyncStorage } from 'react-native';
+import { setError } from './app';
+import config from '../../app-config';
+
+const { api } = config;
+
+export const ActionTypes = {
+  GET_USER: 'GET_USER',
+};
+
+export const getUser = () => async (dispatch) => {
+  const token = await AsyncStorage.getItem('token');
+  axios
+    .get(`${api}/user`, { headers: { authorization: token } })
+    .then((response) => {
+      dispatch({ type: ActionTypes.GET_USER, payload: response.data });
+    })
+    .catch((error) => {
+      dispatch(setError(`Posting failed: ${error.response.data}`));
+    });
+};
