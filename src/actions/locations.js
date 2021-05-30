1dded3c7d13b9a87264f2c8134b297108dd03322
Snow Kang <snow.kang.21@dartmouth.edu>
2021-05-30 03:06:21 -0400

    Added ability to filter feed screen by location search query; still show all locations on first render

--- src/actions/locations.js
+++ src/actions/locations.js
@@ -8,10 +8,10 @@ export const ActionTypes = {
   GET_LOCATIONS: 'GET_LOCATIONS',
 };
 
-export const getLocations = () => async (dispatch) => {
+export const getLocations = (search, locationTitle) => async (dispatch) => {
   return new Promise((resolve, reject) => {
     axios
-      .get(`${api}/locations`)
+      .get(`${api}/locations?search=${search || ''}&location=${locationTitle}`)
       .then((response) => {
         dispatch({ type: ActionTypes.GET_LOCATIONS, payload: response.data });
         resolve();
