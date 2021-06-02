8630bf4521db969280f7bbb67f37ec9e39cdeb7e
Snow Kang <snow.kang.21@dartmouth.edu>
2021-06-02 15:35:16 -0400

    Prevent grid screen from auto-querying on current location

--- src/reducers/locations.js
+++ src/reducers/locations.js
@@ -1,14 +1,14 @@
 import { ActionTypes } from '../actions/locations';
 
 const initialState = {
-  locationsList: [],
+  queriedLocationsList: [],
 };
 
 const LocationsReducer = (state = initialState, action) => {
   const { payload } = action;
   switch (action.type) {
-    case ActionTypes.GET_LOCATIONS:
-      return { ...state, locationsList: payload };
+    case ActionTypes.GET_QUERIED_LOCATIONS:
+      return { ...state, queriedLocationsList: payload };
     default:
       return state;
   }
