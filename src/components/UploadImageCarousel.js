beef1e4fa32c1bacb8ba6359e79a85a17a7f3cb2
Snow Kang <snow.kang.21@dartmouth.edu>
2021-06-08 16:51:31 -0400

    Styling new post page

--- src/components/UploadImageCarousel.js
+++ src/components/UploadImageCarousel.js
@@ -1,12 +1,13 @@
 import React, { useState, useCallback, useRef } from 'react';
 import {
-  View, StyleSheet, Image,
+  View, StyleSheet, Image, Dimensions,
 } from 'react-native';
 import Carousel, { Pagination } from 'react-native-snap-carousel';
 import { connect } from 'react-redux';
-import { bgTertiary } from '../constants/colors';
 import { getUser, deletePost } from '../actions/user';
 import { formatImgUrl } from '../services/imageUpload';
+
+const { width } = Dimensions.get('window');
 
 const UploadImageCarousel = (props) => {
   const {
@@ -21,8 +22,8 @@ const UploadImageCarousel = (props) => {
   const renderItem = useCallback(({ item }) => (
     <View
       style={{
-        height: 320,
-        width: '100%',
+        height: width,
+        width,
       }}
     >
       <Image
@@ -55,13 +56,13 @@ const UploadImageCarousel = (props) => {
     <View>
 
       <View style={styles.card}>
-        <View style={{ height: 350, flexDirection: 'column', alignItems: 'center' }}>
+        <View style={{ height: width, flexDirection: 'column', alignItems: 'center' }}>
           <Carousel
             layout="default"
             ref={ref}
             data={imageUrls}
-            sliderWidth={330}
-            itemWidth={330}
+            sliderWidth={width}
+            itemWidth={width}
             renderItem={renderItem}
             onSnapToItem={(index) => setActiveIndex(index)}
             margin={10}
@@ -75,66 +76,12 @@ const UploadImageCarousel = (props) => {
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: bgTertiary,
-    padding: 15,
-    marginTop: 20,
-    borderRadius: 5,
-  },
-  heading: {
-    flexDirection: 'row',
-    flex: 0,
-    justifyContent: 'space-between',
-  },
-  ellipses: {
-    borderRadius: 20,
-    padding: 10,
-    zIndex: 10,
-    position: 'absolute',
-    left: '90%',
-    bottom: '-2%',
-  },
-  caption: {
-    color: 'white',
-    fontSize: 16,
-    width: '95%',
-    margin: 10,
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalView: {
-    borderRadius: 20,
-    backgroundColor: bgTertiary,
-    alignItems: 'center',
-    shadowColor: '#000',
-    width: '30%',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button2: {
-    backgroundColor: 'rgba(255,255,255,.2)',
-    borderRadius: 20,
-    padding: 10,
-    zIndex: 10,
-    marginTop: 10,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    zIndex: 10,
+    paddingBottom: 20,
   },
   carouselImage: {
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
-    borderRadius: 5,
   },
 });
 
