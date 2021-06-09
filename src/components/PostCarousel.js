10c4e410cabbf46aa8c2f405400b174c16a6c37d
Snow Kang <snow.kang.21@dartmouth.edu>
2021-06-09 18:05:00 -0400

    Styled delete modal

--- src/components/PostCarousel.js
+++ src/components/PostCarousel.js
@@ -4,11 +4,14 @@ import {
 } from 'react-native';
 import Carousel, { Pagination } from 'react-native-snap-carousel';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faEllipsisH, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 import { connect } from 'react-redux';
-import { bgTertiary } from '../constants/colors';
+import {
+  bgTertiary, bgPrimary, accentPurple,
+} from '../constants/colors';
 import LocationHeader from './LocationHeader';
 import { getUser, deletePost } from '../actions/user';
+import fontStyles from '../constants/fonts';
 
 const PostCarousel = (props) => {
   const {
@@ -74,22 +77,29 @@ const PostCarousel = (props) => {
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Pressable
-              style={[styles.button2, styles.buttonClose]}
-              onPress={deletePostAndUpdateUser}
-            >
-              <Text style={{ color: 'white' }}>
-                Delete
-              </Text>
-            </Pressable>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}
-            >
-              <View style={styles.imagesIcon}>
-                <FontAwesomeIcon icon={faTimes} size={24} color="white" />
-              </View>
-            </Pressable>
+            <View style={styles.textHeaderContainer}>
+              <Text style={[fontStyles.smallTextBold, styles.textHeader]}>Just checking...</Text>
+            </View>
+            <Text style={[fontStyles.largeTextRegular, styles.mainText]}>Delete this post?</Text>
+            <Text style={[fontStyles.smallTextRegular, styles.subText]}>This action is final and cannot be undone.</Text>
+            <View style={styles.buttonsContainer}>
+              <Pressable
+                style={[styles.button, { backgroundColor: accentPurple }]}
+                onPress={deletePostAndUpdateUser}
+              >
+                <Text style={fontStyles.smallMediumTextBold}>
+                  Delete
+                </Text>
+              </Pressable>
+              <Pressable
+                style={styles.button}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={fontStyles.smallMediumTextBold}>
+                  Cancel
+                </Text>
+              </Pressable>
+            </View>
           </View>
         </View>
       </Modal>
@@ -165,10 +175,10 @@ const styles = StyleSheet.create({
   },
   modalView: {
     borderRadius: 20,
-    backgroundColor: bgTertiary,
+    backgroundColor: bgPrimary,
     alignItems: 'center',
     shadowColor: '#000',
-    width: '30%',
+    width: '70%',
     shadowOffset: {
       width: 0,
       height: 2,
@@ -177,17 +187,40 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
   },
-  button2: {
-    backgroundColor: 'rgba(255,255,255,.2)',
-    borderRadius: 20,
+  textHeaderContainer: {
+    width: '100%',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey',
+  },
+  textHeader: {
+    textTransform: 'uppercase',
+    color: accentPurple,
+    padding: 20,
+  },
+  mainText: {
     padding: 10,
-    zIndex: 10,
-    marginTop: 10,
+  },
+  subText: {
+    color: 'lightgrey',
+    textAlign: 'center',
+    padding: 20,
+    paddingTop: 0,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    borderBottomStartRadius: 20,
+    borderBottomEndRadius: 20,
+    backgroundColor: bgTertiary,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   button: {
-    borderRadius: 20,
-    padding: 10,
-    zIndex: 10,
+    flex: 1,
+    paddingVertical: 10,
+    alignItems: 'center',
+    borderBottomStartRadius: 20,
   },
   carouselImage: {
     width: '100%',
